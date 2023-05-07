89587bea72564ecc79e8eceafc9a5ce1171a1769
Aleksandra Bozhinoska <aleksandra.bozhinoska@epfl.ch>
2023-05-07 17:35:36 +0200

    Add tests for the disclosure proof

--- test_credential.py
+++ test_credential.py
@@ -109,3 +109,66 @@ def test_failure_issuance_wrong_proof():
     issue_request.pi.s.append(G1.order().random())
 
     sign_issue_request(sk, pk, issue_request, issuer_attributes)
+    
+def test_disclosure_proof_both_issuer_user_attr():
+    sk, pk = generate_key([b"0",b"1"])
+    user_attributes = [Attribute(0, "secret_key", "value0")]
+    issuer_attributes = [Attribute(1, "rest", "true")]
+    issue_req, t = create_issue_request(pk, user_attributes)
+    signature = sign_issue_request(sk, pk, issue_req, issuer_attributes)
+    cred = obtain_credential(pk, signature, t)
+    
+    anonym_cred = cred.anonymize()
+    dis_proof = create_disclosure_proof(pk, anonym_cred, user_attributes, b"1")
+    assert verify_disclosure_proof(pk, dis_proof, b"1", issuer_attributes)
+                    
+def test_disclosure_proof_user_attr_only():
+    sk, pk = generate_key([b"0",b"1"])
+    user_attributes = [Attribute(0, "secret_key", "value0"), Attribute(1, "username", "value1")]
+    issue_req, t = create_issue_request(pk, user_attributes)
+    signature = sign_issue_request(sk, pk, issue_req, [])
+    cred = obtain_credential(pk, signature, t)
+    
+    anonym_cred = cred.anonymize()
+    dis_proof = create_disclosure_proof(pk, anonym_cred, user_attributes, b"1")
+    assert verify_disclosure_proof(pk, dis_proof, b"1", [])              
+
+def test_disclosure_proof_issuer_attr_only():
+    sk, pk = generate_key([b"0",b"1"])
+    issuer_attributes = [Attribute(0, "key0", "value0"), Attribute(1, "key1", "value1")]
+    issue_req, t = create_issue_request(pk, [])
+    signature = sign_issue_request(sk, pk, issue_req, issuer_attributes)
+    cred = obtain_credential(pk, signature, t)
+    
+    anonym_cred = cred.anonymize()
+    dis_proof = create_disclosure_proof(pk, anonym_cred, [], b"1")
+    assert verify_disclosure_proof(pk, dis_proof, b"1", issuer_attributes)
+    
+def test_disclosure_proof_custom_disclosure_1():
+    sk, pk = generate_key([b"0",b"1",b"2"])
+    attr_1 = Attribute(0, "username", "value0")
+    attr_2, attr_3 = Attribute(1, "rest", "true"), Attribute(2, "dojo", "true")
+    user_attributes = [attr_1]
+    issuer_attributes = [attr_2, attr_3]
+    issue_req, t = create_issue_request(pk, user_attributes)
+    signature = sign_issue_request(sk, pk, issue_req, issuer_attributes)
+    cred = obtain_credential(pk, signature, t)
+    
+    anonym_cred = cred.anonymize()
+    dis_proof = create_disclosure_proof(pk, anonym_cred, [attr_1, attr_2], b"1")
+    assert verify_disclosure_proof(pk, dis_proof, b"1", [attr_3])
+
+def test_disclosure_proof_custom_disclosure_2():
+    sk, pk = generate_key([b"0",b"1",b"2",b"3",b"4"])
+    attr_1, attr_2 = Attribute(0, "secret_key", "value0"), Attribute(1, "username", "value1")
+    attr_3, attr_4, attr_5 = Attribute(2, "rest", "true"), Attribute(3, "dojo", "true"), Attribute(4, "bar", "false")
+    user_attributes = [attr_1, attr_2]
+    issuer_attributes = [attr_3, attr_4, attr_5]
+    issue_req, t = create_issue_request(pk, user_attributes)
+    signature = sign_issue_request(sk, pk, issue_req, issuer_attributes)
+    cred = obtain_credential(pk, signature, t)
+    
+    anonym_cred = cred.anonymize()
+    dis_proof = create_disclosure_proof(pk, anonym_cred, [attr_1, attr_2, attr_3], b"1")
+    assert verify_disclosure_proof(pk, dis_proof, b"1", [attr_4, attr_5])       
+    
